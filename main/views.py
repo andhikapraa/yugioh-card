13ccc591b1c265a4c8ca3397001865a1f38ef776
Muhammad Andhika Prasetya <andhikapraa@gmail.com>
2023-09-10 20:27:21 +0700

    Memperbaiki bug pada views.py

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        "title": "Yu-Gi-Oh! Card Collection",
+        "project": "Yu-Gi-Oh! Card Collection",
         "name": "Muhammad Andhika Prasetya",
         "npm": "2206031302",
         "class": "PBP C",
